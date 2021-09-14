1bb3b93d816338bbf717d1c31bc200d5b0b29caf
okaycodes <fo.okenana@gmail.com>
2021-09-14 23:15:47 +0100

    importing necesarry components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import MainContent from "./MainContent"
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
