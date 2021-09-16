5f565e2d01dded6113e64fbb107b81831e8c496f
okaycodes <fo.okenana@gmail.com>
2021-09-16 17:54:07 +0100

    add header content

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,13 @@ import React from "react"
 
 function Header(){
   return(
-    <div>
-      Film Search App in react
-    </div>
+    <header>
+      <img
+          src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
+          alt="Problem?"
+      />
+      <p>Meme Generator</p>
+    </header>
   )
 }
 
