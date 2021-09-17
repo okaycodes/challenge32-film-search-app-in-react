257cf00a02711276db91f2a0a97f2c94bf5eda81
okaycodes <fo.okenana@gmail.com>
2021-09-17 20:32:04 +0100

    refactor out header.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import MemeGenerator from "./components/MemeGenerator"
 import Header from './components/Header'
-import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MemeGenerator />
-      <Footer />
     </div>
   );
 }
